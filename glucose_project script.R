8b9b4b54f82dd22e6c21aaa8616131906b9a0eb9
VNH <hovannhut1@gmail.com>
2020-06-16 19:17:05 +0200

    2nd

---
+++ glucose_project script.R
@@ -0,0 +1,28 @@
+
+
+###################
+#CAPSTONE PROJECT 2
+###################
+#GLYCEMIA PROJECT
+
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+if(!require(kableExtra)) install.packages("kableExtra")
+if(!require(tidyr)) install.packages("tidyr")
+if(!require(stringr)) install.packages("stringr")
+if(!require(forcats)) install.packages("forcats")
+if(!require(ggplot2)) install.packages("ggplot2")
+
+                             
+# All libraraies needed
+library(dplyr)
+library(tidyverse)
+library(kableExtra)
+library(tidyr)
+library(stringr)
+library(forcats)
+library(ggplot2)
