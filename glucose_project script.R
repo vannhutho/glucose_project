447f011a856006598cef1262e73f8e8e91d22b60
vannhutho <64103047+vannhutho@users.noreply.github.com>
2020-06-21 00:40:42 +0200

    last version

--- glucose_project script.R
+++ glucose_project script.R
@@ -3,7 +3,7 @@
 ###################
 #CAPSTONE PROJECT 2
 ###################
-#GLYCEMIA PROJECT
+#GLUCOSE PROJECT
 
 
 # Note: this process could take a couple of minutes
@@ -35,17 +35,20 @@ library(corrplot)
 library(mlr)
 library(caret)
 
-\newpage
+#\newpage
 
 #1 EXECUTIVE SUMMARY
-The aim of this project is to show if the age and the weight influence on the glycemia (glucose). 
-The Glycemia refers to the concentration of sugar or glucose in the blood. 
-In many of the European countries blood glucose or sugar is also measured as millimol per decilitre (mmol/dl). 
-These measurements are referred to as the SI units.
-If the blood glucose level above 6.1 mmol/l or 1.10 g/l (hyperglycemia and less than 3.5 mmol/l or 0.54 g/l (hypoglycemia)
-When fasting blood glucose is above 7 mmol/l (1.26 g/l), the diagnosis of diabetes is made. 
+#The aim of this project is to show if the age and the weight influence on the glycemia (glucose). 
+#The Glycemia refers to the concentration of sugar or glucose in the blood. 
+#In many of the European countries blood glucose or sugar is also measured as millimol per decilitre (mmol/dl). 
+#These measurements are referred to as the SI units.
+#If the blood glucose level above 6.1 mmol/l or 1.10 g/l (hyperglycemia and less than 3.5 mmol/l or 0.54 g/l (hypoglycemia)
+#When fasting blood glucose is above 7 mmol/l (1.26 g/l), the diagnosis of diabetes is made. 
 
 #2 METHODS AND ANALYSIS
+#In this section, I'll present the methods and the analysis of the data. Before continuing the process, 
+#it's really important to discover the variables (names, types, counts, lenght etc.)
+
 #Loading raw Data set
 glucose <- read.csv("C:/Users/vnh/Desktop/glc.csv")
 
@@ -54,23 +57,23 @@ str(glucose)
 #> This dataset contains 10 variables and 284 observations.
 
 #Type of variable :
-- id "integer" = unique identification value per patient
-- chol "integer" = unique identification value of cholesterol per patient
-- glyhb "numeric" = unique identification value of glycemia per patient
-- location "factor" = specific city per patient
-- age "integer" = age of patient
-- gender "factor" = gender female or male
-- height "numeric" = unique identification value per patient in cm
-- weight "numeric" = unique identification value per patient in kg
-- waist "integer" = unique identification value per patient in cm
-- age_cat "factor" = age groups 20-39/40-59/60+ years
+#- id "integer" = unique identification value per patient
+#- chol "integer" = unique identification value of cholesterol per patient
+#- glyhb "numeric" = unique identification value of glycemia per patient
+#- location "factor" = specific city per patient
+#- age "integer" = age of patient
+#- gender "factor" = gender female or male
+#- height "numeric" = unique identification value per patient in cm
+#- weight "numeric" = unique identification value per patient in kg
+#- waist "integer" = unique identification value per patient in cm
+#- age_cat "factor" = age groups 20-39/40-59/60+ years
 
 
 #checking for missing variables
 sapply(glucose, function(x) sum(is.na(x)))
 
 #looking closer attributes and data values for:
-#glyhb
+#glyhb variable
 summary(glucose$glyhb)
 skewness(glucose$glyhb)
 kurtosis(glucose$glyhb)
@@ -84,7 +87,7 @@ ggplot(dfglyhb, aes(x = glucose$glyhb), binwidth = 2) +
 boxplot(glucose$glyhb)
 
 
-#weight
+#weight variable
 summary(glucose$weight)
 skewness(glucose$weight)
 kurtosis(glucose$weight)
@@ -95,7 +98,7 @@ ggplot(dfweight, aes(x = glucose$weight), binwidth = 2) +
   geom_density(colour = 'blue') + xlab(expression(bold('weight'))) + 
   ylab(expression(bold('Density')))
 
-#age
+#age variable
 summary(glucose$age)
 skewness(glucose$age)
 kurtosis(glucose$age)
@@ -107,7 +110,7 @@ ggplot(dfage, aes(x = glucose$age), binwidth = 2) +
   ylab(expression(bold('Density')))
 
 
-#chol
+#chol variable
 summary(glucose$chol)
 skewness(glucose$chol)
 kurtosis(glucose$chol)
@@ -133,31 +136,41 @@ corrglucose <- cor(glucose1[,2:6])
 corrplot(corrglucose, order = "hclust", tl.cex = 0.7)
 
 # Linear regression
-cor(glucose$glyhb, glucose$weight, method = c("pearson", "kendall", "spearman"))
+#Linear regression between glycemia and weight:
+#Following this score and the scatter plot, there is a weak positive linear relationships between these variables.
+cor(glucose$glyhb, glucose$weight, method = c("pearson", "kendall", "spearman")) #> 0.098
 glycemia <- glucose$glyhb
 weight <- glucose$weight
 scatter.smooth(x=glycemia, y=weight, main="glycemia ~ kg")  # scatterplot
 
-cor(glucose$glyhb, glucose$chol, method = c("pearson", "kendall", "spearman"))
+
+#Linear regression between glycemia and cholesterol:
+#As we can see, there is a weak positive linear regression.
+cor(glucose$glyhb, glucose$chol, method = c("pearson", "kendall", "spearman")) #> 0.113
 glycemia <- glucose$glyhb
 cholesterol <- glucose$chol
 scatter.smooth(x=glycemia, y=cholesterol, main="glycemia ~ mmol/dl")  # scatterplot
 
-cor(glucose$glyhb, glucose$age, method = c("pearson", "kendall", "spearman"))
+#Linear regression between glyemia and age:
+#Here we see a slight positive correlation between these variables.
+cor(glucose$glyhb, glucose$age, method = c("pearson", "kendall", "spearman")) #> 0.307
 glycemia <- glucose$glyhb
 age <- glucose$age
 scatter.smooth(x=glycemia, y=age, main="glycemia ~ age")  # scatterplot
 
-cor(glucose$glyhb, glucose$waist, method = c("pearson", "kendall", "spearman"))
+#Linear regression between glyemia and wast:
+#The score and the scatter plot show us a slight positive correlation between these variables.
+cor(glucose$glyhb, glucose$waist, method = c("pearson", "kendall", "spearman")) #> 0.171
 glycemia <- glucose$glyhb
 scatter.smooth(x=glycemia, y=age, main="glycemia ~ waist")  # scatterplot
+
 
 #Data processing
 
 #Principal Components Analysis (PCA) transform
-Principal component analysis is a statistical technique that is used to analyze the interrelationships among a large number of variables and to explain these variables in terms of a smaller number of variables, called principal components, with a minimum loss of information.
-It constructs a set of orthogonal (non-collinear, uncorrelated, independent) variables and is used for making predictive models
-glucose.pca <- prcomp(glucose1, center=TRUE, scale=TRUE)
+#Principal component analysis is a statistical technique that is used to analyze the interrelationships among a large number of variables and to explain these variables in terms of a smaller number of variables, called principal components, with a minimum loss of information.
+#It constructs a set of orthogonal (non-collinear, uncorrelated, independent) variables and is used for making predictive models
+#glucose.pca <- prcomp(glucose1, center=TRUE, scale=TRUE)
 plot(glucose.pca, type="l", main='')
 grid(nx = 6, ny = 5)
 title(main = "Principal components weight", sub = NULL, xlab = "Components")
@@ -186,8 +199,8 @@ test_index <- createDataPartition(y = glucose$glyhb, p = 0.65, list = FALSE)
 train_set <- glucose[test_index,]
 test_set <- glucose[-test_index,]
 
-nrow(train_set)
-nrow(test_set)
+nrow(train_set) #>[1] 187
+nrow(test_set)  #>[1] 97
 
 #Overview edx and validation dataset
 
@@ -208,7 +221,7 @@ head(test_set) %>%
 #Naive Bayes
 #Average of all glycemia
 mu_hat <- mean(glucose$glyhb)
-mu_hat
+mu_hat #> 4.687
 
 #Predict the RMSE on the validation set
 rmse_mean_model_result <- RMSE(test_set$glyhb, mu_hat)
@@ -216,7 +229,7 @@ rmse_mean_model_result
 
 #Finally a dataframe of result
 results <- data.frame(model="Naive Mean-Baseline Model", RMSE=rmse_mean_model_result)
-results
+results #> 4.687
 
 results %>% 
   kable() %>%
@@ -265,14 +278,15 @@ data.frame( R2 = R2(predictions, test_set$glyhb),
             MAE = MAE(predictions, test_set$glyhb))
 RMSE_K_FOLD_CROSS_VALIDATION <- RMSE(predictions, test_set$glyhb)
 #When comparing two models, the one that produces the lowest test sample RMSE is the preferred model.
-the RMSE and the MAE are measured in the same scale as the outcome variable. Dividing the RMSE by the average value of the outcome variable will give you the prediction error rate, which should be as small as possible:
+#The RMSE and the MAE are measured in the same scale as the outcome variable. Dividing the RMSE by the average value of the outcome variable will give you the prediction error rate, which should be as small as possible:
   
 RMSE(predictions, test_set$glyhb)/mean(test_set$glyhb)
 #Note that, the validation set method is only useful when you have a large data set that can be partitioned. 
 #Therefore, the test error rate can be highly variable, depending on which observations are included in the training set and which observations are included in the validation set.
 
+
 #K-fold cross-validation
-Define training control
+#Define training control
 set.seed(200, sample.kind="Rounding")
 train.control <- trainControl(method = "cv", number = 10)
 #Train the model
@@ -282,6 +296,8 @@ model <- train(glyhb ~., data = glucose, method = "lm",
 print(model)
 
 #Repeated K-fold cross-validation
+#The process of splitting the data into k-folds could be repeatedas many times as wished and called repeated k-fold cross validation.
+#The final model error is taken as the mean error from the number of repeats.
 #Define training control
 set.seed(200)
 train.control2 <- trainControl(method = "repeatedcv", 
@@ -297,5 +313,7 @@ print(model2)
 results <- results %>% add_row(model="K-fold cross-validation", RMSE=RMSE_K_FOLD_CROSS_VALIDATION)
 results
 
+
+#APPENDIX
 sessionInfo()
 
